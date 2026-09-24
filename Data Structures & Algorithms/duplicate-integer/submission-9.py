d59d76a56d168f2ab1cf83ceafc77f886b65b20c
Vamsi Konakanchi <39833739+vmskonakanchi@users.noreply.github.com>
2026-09-24 11:11:50 +0530

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+
+        for n in nums:
+            if n in seen:
+                return True
+            seen[n] = True
+
+        return False
